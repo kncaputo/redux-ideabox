7ac2550ca425321d8e7573977289c919ec81fde7
Kara Caputo <kncaputo@gmail.com>
2021-01-27 16:26:37 -0500

    Add store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
 import App from './components/App';
 import './index.css';
 
+const store = createStore(rootReducer, composeWithDevTools());
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
